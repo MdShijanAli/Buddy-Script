1deeb3d22c4b48e7633f2c22da34cb83b67d7219
MdShijanAli <shijan135@gmail.com>
2026-04-03 18:16:10 +0600

    feat: add additional profile and public story components to FeedMiddle

--- src/components/FeedMiddle.tsx
+++ src/components/FeedMiddle.tsx
@@ -5,7 +5,11 @@ import postImage from "../assets/images/post_img.png";
 import reactImage1 from "../assets/images/react_img1.png";
 import reactImage2 from "../assets/images/react_img2.png";
 import cardPP11Image from "../assets/images/card_ppl1.png";
+import cardPP12Image from "../assets/images/card_ppl2.png";
+import cardPP13Image from "../assets/images/card_ppl3.png";
+import cardPP14Image from "../assets/images/card_ppl4.png";
 import timeLineImage from "../assets/images/timeline_img.png";
+import miniPicImage from "../assets/images/mini_pic.png";
 
 export default function FeedMiddle() {
   const { toggleDropdown, isDropdownOpen } = useMultipleDropdowns();
@@ -40,7 +44,7 @@ export default function FeedMiddle() {
           </div>
           <div className="row">
             <div className="col-xl-3 col-lg-3 col-md-4 col-sm-4 col">
-              <div className="_feed_inner_profile_story _b_radious6">
+              <div className="_feed_inner_profile_story _b_radious6 ">
                 <div className="_feed_inner_profile_story_image">
                   <img
                     src={cardPP11Image}
@@ -66,6 +70,75 @@ export default function FeedMiddle() {
                       </button>
                     </div>
                     <p className="_feed_inner_story_para">Your Story</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-3 col-lg-3 col-md-4 col-sm-4 col">
+              <div className="_feed_inner_public_story _b_radious6">
+                <div className="_feed_inner_public_story_image">
+                  <img
+                    src={cardPP12Image}
+                    alt="Image"
+                    className="_public_story_img"
+                  />
+                  <div className="_feed_inner_pulic_story_txt">
+                    <p className="_feed_inner_pulic_story_para">
+                      Ryan Roslansky
+                    </p>
+                  </div>
+                  <div className="_feed_inner_public_mini">
+                    <img
+                      src={miniPicImage}
+                      alt="Image"
+                      className="_public_mini_img"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-3 col-lg-3 col-md-4 col-sm-4 _custom_mobile_none">
+              <div className="_feed_inner_public_story _b_radious6">
+                <div className="_feed_inner_public_story_image">
+                  <img
+                    src={cardPP13Image}
+                    alt="Image"
+                    className="_public_story_img"
+                  />
+                  <div className="_feed_inner_pulic_story_txt">
+                    <p className="_feed_inner_pulic_story_para">
+                      Ryan Roslansky
+                    </p>
+                  </div>
+                  <div className="_feed_inner_public_mini">
+                    <img
+                      src={miniPicImage}
+                      alt="Image"
+                      className="_public_mini_img"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-3 col-lg-3 col-md-4 col-sm-4 _custom_none">
+              <div className="_feed_inner_public_story _b_radious6">
+                <div className="_feed_inner_public_story_image">
+                  <img
+                    src={cardPP14Image}
+                    alt="Image"
+                    className="_public_story_img"
+                  />
+                  <div className="_feed_inner_pulic_story_txt">
+                    <p className="_feed_inner_pulic_story_para">
+                      Ryan Roslansky
+                    </p>
+                  </div>
+                  <div className="_feed_inner_public_mini">
+                    <img
+                      src={miniPicImage}
+                      alt="Image"
+                      className="_public_mini_img"
+                    />
                   </div>
                 </div>
               </div>
